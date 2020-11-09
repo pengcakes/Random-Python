49d3c6c79df4648e888bbb99e0a3a09dea20a88b
Austin <austinpeng97@gmail.com>
2020-11-09 06:58:34 -0600

    c

--- Practice/runtime.py
+++ Practice/runtime.py
@@ -1,4 +1,47 @@
+"""
+Big-O Anal
 
+▪ Ideal algorithm - O(1) - Linear Search, Binary Search,
+    Bubble Sort, Selection Sort, Insertion Sort, Heap Sort, Shell Sort.
+▪ Logarithmic - O(log n) - Merge Sort.
+▪ Linear algorithm - O(n) - Quick Sort.
+▪ Linearithmic - O(n * log n)
+▪ Sub-linear algorithm - O(n+k) - Radix Sort.
+▪ Quadratic algorithm - O(n2) 	Selection sort
+
+* Big(O) is upper bound of runtime. *
+
+
+Log(n) and n*log(n) never really made sense to me...
+
+Logarithm - the inverse of an exponential function. 2^x --> log2x. Mirror image of exponential on a graph
+
+*Most common attribute* - multiple choices to choose from, only need to pick one.
+Most common example - phone book, don't need to loop through entire book, just go by alphabet
+Runtime - Time goes up linearly while n (search space) goes up logarithmically.
+
+
+*Anything w/ log(n) (in terms of sorting functions) will usually only apply to datasets that are already sorted / datasets we already have info on.
+
+*Merge Sort (n log n)
+
+https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+
+(n log n) because
+
+
+"""
+
+
+
+
+
+
+
+
+"""
+EXAMPLES
+"""
 """
 too slow, runs a sum() interates the whole list every iteration
 times out 12000ms
